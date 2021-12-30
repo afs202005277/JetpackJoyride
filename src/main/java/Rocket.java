f3fc70edb8ff377c2ea39aa0a4848a9c3531ab67
Bemdito <64276888+Bemdito@users.noreply.github.com>
2021-12-30 16:02:40 +0000

    Criada a funcao de mover o rocket

--- src/main/java/Rocket.java
+++ src/main/java/Rocket.java
@@ -3,19 +3,18 @@
 
 
 public class Rocket extends Obstacle{
-    private int x;
-    private int y;
+    private Position position;
 
-    public int getX() {return x;}
-    public void setX(int x) {this.x = x;}
-    public int getY() {return y;}
-    public void setY(int y) {this.y = y;}
+    public int getX() {return position.getX();}
+    public void setX(int x) {position.setX(x);}
+    public int getY() {return position.getY();}
+    public void setY(int y) {position.setY(y);}
 
     public Rocket() {
-        y = (int) (Math.random() * (74 - 1)) + 1;
-        x = 132;
+        int y = (int) (Math.random() * (74 - 1)) + 1;
+        position = new Position(132,y);
     }
     public void moveRocket() {
-
+        position.setX(position.getX()-1);
     }
 }
