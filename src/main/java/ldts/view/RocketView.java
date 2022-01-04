1ba65575df9f42523bb1edb2859105e876bf2deb
Bemdito <64276888+Bemdito@users.noreply.github.com>
2022-01-04 17:24:19 +0000

    Pequena correção

--- src/main/java/ldts/view/RocketView.java
+++ src/main/java/ldts/view/RocketView.java
@@ -13,7 +13,7 @@ public class RocketView extends View {
     public void draw(Position pos) throws IOException {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#000C66"));
         graphics.setForegroundColor(TextColor.Factory.fromString("#ff1f1f"));
-        graphics.putString(pos.getX(), pos.getY(), "xX");
+        graphics.putString(pos.getX(), ROWS - pos.getY(), "xX");
         refresh();
     }
 }
