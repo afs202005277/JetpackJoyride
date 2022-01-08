46b639417ec595c20fd4ae6fea05cf3eca38e8dd
afs202005277 <up202005277@up.pt>
2022-01-08 16:46:19 +0000

    It works

--- src/test/java/ldts/CommandTest.java
+++ src/test/java/ldts/CommandTest.java
@@ -1,16 +1,14 @@
 package ldts;
 
-import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 import ldts.control.Command;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class CommandTest {
@@ -18,14 +16,7 @@ public class CommandTest {
     private Command command;
     @BeforeEach
     public void getTestScreen() throws IOException {
-        TerminalSize terminalSize = new TerminalSize(40, 20);
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-        Terminal terminal = null;
-        terminal = terminalFactory.createTerminal();
-        screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null);   // we don't need a cursor
-        screen.startScreen();             // screens must be started
-        screen.doResizeIfNecessary();     // resize screen if necessary
+        screen = Mockito.mock(Screen.class);
     }
 
     @BeforeEach
@@ -47,10 +38,23 @@ public class CommandTest {
     }
 
     @Test
-    public void inputReaderTest() throws IOException, AWTException {
-        Robot robot = new Robot();
-        robot.keyPress(32); // 32 is the code of space bar
+    public void inputReaderTest() throws IOException {
+        KeyStroke k = Mockito.mock(KeyStroke.class);
+        Mockito.when(k.getCharacter()).thenReturn(' ');
+        Mockito.when(k.getKeyType()).thenReturn(KeyType.Character);
+        Mockito.when(screen.readInput()).thenReturn(k);
+        //When space is pressed
         command.inputReader(screen);
-        Assertions.assertEquals(' ', command.getKey());
+        Assertions.assertEquals(k.getCharacter(), command.getKey());
+
+        //When any other char is pressed
+        Mockito.when(k.getCharacter()).thenReturn('a');
+        command.inputReader(screen);
+        Assertions.assertEquals('a', command.getKey());
+
+        //When any other key is pressed (not character)
+        Mockito.when(k.getKeyType()).thenReturn(KeyType.ArrowDown);
+        command.inputReader(screen);
+        Assertions.assertEquals('0', command.getKey());
     }
 }
