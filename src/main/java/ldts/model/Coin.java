5bb9b42467ec266b0d099d2d1efd1d4ede12967d
Pedro Fonseca <pedrojgfonseca@gmail.com>
2022-01-13 19:31:21 +0000

    Added move() method to Coin

--- src/main/java/ldts/model/Coin.java
+++ src/main/java/ldts/model/Coin.java
@@ -1,8 +1,14 @@
 package ldts.model;
 
+import ldts.view.View;
+
 public class Coin extends Element {
 
     public Coin(Position position) {
         super(position);
+    }
+
+    public void move() {
+        position = new Position(position.getX()-1, position.getY());
     }
 }
